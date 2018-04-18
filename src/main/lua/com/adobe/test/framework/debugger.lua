275eef401455cd5906fef2576b2d26d44181fcef
trifan <trifan@adobe.com>
2018-04-18 17:44:50 +0300

    update

--- src/main/lua/com/adobe/test/framework/debugger.lua
+++ src/main/lua/com/adobe/test/framework/debugger.lua
@@ -123,7 +123,33 @@ function Debugger:breakPointReached(file, line)
         end
     end)
 
-    ngx.sleep(20)
+    local sleep = function()
+        local ffi = require("ffi")
+        ffi.cdef[[
+            void Sleep(int ms);
+            int poll(struct pollfd *fds, unsigned long nfds, int timeout);
+            ]]
+
+        local sleep
+        if ffi.os == "Windows" then
+            function sleep(s)
+                ffi.C.Sleep(s*1000)
+            end
+        else
+            function sleep(s)
+                ffi.C.poll(nil, 0, s*1000)
+            end
+        end
+
+        for i=1,160 do
+            io.write("."); io.flush()
+            sleep(2)
+        end
+        io.write("\n")
+    end
+
+    sleep()
+
 
 --    while not self.continueExecution do
 --        ngx.sleep(2)
