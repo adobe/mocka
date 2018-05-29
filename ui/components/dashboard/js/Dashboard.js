cd60444cd57efcab56d49fb9877aef687741e04a
trifan <trifan@adobe.com>
2018-05-29 23:26:42 +0300

    update dashboard

---
+++ ui/components/dashboard/js/Dashboard.js
@@ -0,0 +1,14 @@
+define([], function() {
+
+    function DashBoard() {}
+
+    DashBoard.prototype.init = function() {
+
+    };
+
+    DashBoard.prototype.start = function() {
+
+    };
+
+    return DashBoard;
+});
