77c64d33e163c6461633f0c3918fc5c827dc044d
trifan <trifan@adobe.com>
2018-07-04 10:04:47 +0300

    if condition did not take into account the DOCS_FOLDER env variable

--- docker/generate_docs.sh
+++ docker/generate_docs.sh
@@ -1,6 +1,6 @@
 echo " Running ldoc for ${LUA_LIBRARIES} "
 
-if [ -d "docs/style" ]; then
+if [ -d "${DOCS_FOLDER:-docs}/style" ]; then
     ldoc -B "${LUA_LIBRARIES}" -d "${DOCS_FOLDER:-docs}" -s "${DOCS_FOLDER:-docs}/style" -a
 else
     ldoc -B "${LUA_LIBRARIES}" -d "${DOCS_FOLDER:-docs}" -a
