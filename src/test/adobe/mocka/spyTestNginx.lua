4d3ba569fdd00331339126cbc6c9473a55541547
Corina Purcarea <purcarea@adobe.com>
2021-05-11 11:57:38 +0300

    Reset spies (#127)
    * reset spy
    
    * wrong condition

--- src/test/adobe/mocka/spyTestNginx.lua
+++ src/test/adobe/mocka/spyTestNginx.lua
@@ -13,22 +13,37 @@ afterEach(function()
     isNgx = false
 end)
 
-test('override lazy spy', function()
+test('nginx - override lazy spy nginx', function()
     spy("cjson", "decode", function()
         return "overwritten"
     end)
 
     local cjson = require "cjson"
     assertEquals(cjson.decode(), "overwritten")
+
+    reset_spy("cjson", "decode")
+
+    local payload = '{"key":"test"}'
+    local decoded = {}
+    decoded["key"] = "test"
+
+    local cjson = require "cjson"
+    assertEquals(cjson.decode(payload), decoded)
 end)
 
-test('override preloaded module spy', function()
+test('nginx - override preloaded module spy nginx', function()
     spy("src.test.adobe.mocka.some_class", "testing", function()
         return "overwritten preloaded module"
     end)
     assertEquals(some_class:testing(), "overwritten preloaded module")
+
+    reset_spy("src.test.adobe.mocka.some_class", "testing")
+    assertEquals(some_class:testing(), "real")
 end)
 
-test('just beforeEach on preload', function()
+test('nginx - just beforeEach on preload nginx', function()
     assertEquals(some_class:testing(), "beforeEach")
+
+    reset_spy("src.test.adobe.mocka.some_class", "testing")
+    assertEquals(some_class:testing(), "real")
 end)
